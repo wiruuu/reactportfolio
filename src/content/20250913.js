12e0f18e0bf4245152e560f52fcf34d81bdd5040
Ethan Choi <ethanchoi@Ethans-MacBook-Pro.local>
2025-09-15 03:50:10 +0800

    sep14

--- src/content/20250913.js
+++ src/content/20250913.js
@@ -2,6 +2,92 @@ export const content = `
 
 ### Premise
 
+Consider the following (abriged) scenario:
+
+<div className="border-l-4 border-primary/70 pl-4">
+<div className="text-md">
+
+  I want my daughter to study \*insert reputable subject\* at an \*insert reputable university in the US\*, but she doesn't have any published research papers or internships or work shadowing yet. She is only 13 yet she is behind compared to her classmates. Parents with kids her age are sending their kids to summer programmes and teenagers her age have published research papers and are doing competitions! How do I develop a roadmap for the next 5-6 years to secure my kid's chance at \*insert reputable university\*?
+</div>
+</div>
+
+The typical answer is to develop a niche in a field where your demographic is not oversaturated in, and endeavour to explore something creative, special, awe-inspiring, and produce something that produces unique impact, whether it be a piece of comparative intersectional marginalised cross-cultural meta-linguistic research, or a soup kitchen. Or they must endeavour to complete olympiads, produce or directly facilitate a startup, join a feeder programme, and to package and position oneself within that niche. 
+
+Why these name-brand universities? Easy question. Graduates of these universities go on to lead productive lives with high-paying jobs. Many then make this fallacious argument: if one thrives in a high-pressure university with sky-high expectations, why shouldn't I expect them to thrive in the workplace? 
+
+A lot to unpack. In comparison, John F. Kennedy's Harvard application ends with "Then too, I would like to go to the same college as my father. To be a "Harvard man" is an enviable distinction, and one that I sincerely hope I shall attain".
+
+The typical objection to this phenomena is that students are encouraged to become machines that churn out creative, impressive novelties at the expense of their own interests; their formative years are constructed to serve the eyes of an admissions officer who takes at most a cursory glance at their profile. That the majority of students with "normal" achievements and who have not employed a personal admissions tutor are viewed as cookie-cutter. Evidently those who have branded themselves by appealing to a niche have encrusted diamonds and ornamental trinkets among the cookies they have cut for themselves.
+
+This is not the problem. The problem is with the Herculean objectives students compare among themselves to achieve, and the moral collapse that may inadvertently arise as a consequence. It creates a generation of high-performers that eschew kindness and virtue for moral self-worth and self-superiority as each high performer races to become the cream of the crop.
+
+### Never enough
+
+When we are so intent on proving to the world (or to the admissions officer) that our contributions add value, we focus too much on numbers and titles instead of real accomplishments. While some may say high-schoolers have no business gathering angel investors for their pre-seed startup, I hold that students focus too much on the achieved value of said endeavours instead of the impact they bring. An accomplishments-run system is a gameified system. Not to mention the whole ordeal with test-optionality, early decision, financial aid, need-blindness, and DEI. Some univerisities run rules such as giving students one-time offer of admission if they can commit on the spot to attend and shun all other prospects.
+
+The potential acculturative prospects of this system is very dangerous for a teenager's moral upbringing.
+
+When college admissions focus on the content of students' precocious acheivements, students often fall into the trap of determining their worth based on the quantity and uniqueness of their academic and extracurricular endeavours. They determine their worth, often compared to some base metric, based on how many internships and connections they have. At some point this becomes vitriolic as students look down upon each other or are jealous of each other if they have less or more "in-game points" than themselves. Evidently so, students and parents will look down upon schools in their geographical region who have produced students with, on aggregate, less in-game points. 
+
+Students have it ingrained in themselves that "they haven't achieved enough". Passed three uni-level courses? Somebody else has excelled at five. Produced one piece of research with a PHD student? Somebody else has co-written a monograph with a professor. Here learning or achieving isn't an end in itself. It always serves a greater purpose, especially with how nebulous the application criteria is. 
+
+The worst thing about incessant achievement is that there is no margin for error - or rather, one does not know what the margin is. It is ludicrous for students to decide what academic career or path they want to set themselves on at a pre-pubescent age to even get a hair's chance of getting an offer from their dream university. There is no try. Every path is calculated from a young age - an age where sufficiently few students have started thinking about university such that their extracurricular empire would look grandiose enough come applications season. Students become overwhelmed, anxious, and consumed by the belief that even the most minute of failures are fatal. Worst of all, students' motivation for undergoing endeavours have appealed to instrumental reasons such as "will this get me an internship", "will this get me into \*insert name-brand university\*" instead of intrinsic satisfaction and passion.
+
+Most Ivy League schools have been served negligence lawsuits for students who have committed suicide; a study has shown that<a href="https://pmc.ncbi.nlm.nih.gov/articles/PMC9331452/" className="underline hover:text-primary:>around half</a> of all undergraduate students are suffering from moderate to severe depression. I am appalled to announce that this number is higher for prestigious universities. It wouldn't be unwise to suggest the causative relationship between this statistic and the precipitous, error-free path high-performing students are trained to adhere to.
+
+Students cannot explore multiple interests as admissions officers are looking for those with a specific, unbridled edge in one field instead of the typical well-rounded student. Many students eschew their true passions as they trade their passions for their envisionment of the rules of the game.
+
+College applications filter out intelligent students who have not branded themselves in a way that gets the most bang for their buck, or those who are unaware of the rules of the game. I personally know a few individuals who fall under this umbrella. Any student can display signs of emotional intelligence, relationship skills, passion etc. (indispensable soft skills), but who is to say that they are? Are students with more accolades more passionate? Universities say they want to see each candidate as who they are; in reality, they segregate by loosely defined tenets of a shaky meritocratic system.
+
+What can universities do about it? Not much. We obviously cannot get rid of selective admission. We could hold universities accountable for their processes and their decisions, and ask them to state their admissions criteria instead of having them nebulously admit that they view a person's application holistically and seek to understand how they would develop and thrive at their university. But this would create a more narrow scope of students trying to curry favour with a rubric by presenting some version of themselves faithful to the new rules of the game.
+
+We could hold them accountable for favouring the children of alumni and favouring those who donate buildings. Everybody applying should have a fair value of the true costs of application instead of these costs being blurred behind an ambiguously gameified system. 
+
+<div className="border-l-4 border-primary/70 pl-4">
+But we cannot eschew the fact that the more well-off you are, the more you understand the rules of the game, and the more resources you are equipped with to tackle said game. Any meritocracy has worked in a way that disproportionately favours the affluent. We have constructed, prima facie, a system of castes where the elite believe that they have earned their right in their caste due not to privilege but to pure diligence.
+</div>
+
+### Soup kitchens, accolades, prizes
+
+There are many students who build their own NGO or soup kitchen to prove to admissions officers that they at least have something purportedly worthwhile on their CV. Most of these are evident products of artifice and hold little value; they represent the student's acknowledgement of the rules of the game and, using their tools at hand, their attempt at playing it. Admissions officers know this and have their hand on the "reject application" button. 
+
+How do we differentiate between students who are geniunely passionate in engaging their local community to produce impact, and those who just love Harvard too much? By the content of their leadership work or organisation. Those who are genuinely passionate are transparent, regular, and genuine with the impact they deliver. Those who do not fixate their mind on measurable quantities or achievements. Unfortunately there is a large overlap between this type of extracurricular work, and endeavours that top univeristies deem unworthy of their acceptance, especially for those who are not seasoned interpreters of the rules of the game. 
+
+What is success? Success in school is not success in life. There is plenty of room for comparison in one's formative years, and even beyond one's formative years. Students take no hesitation in flocking to internship applications as early as their freshman year of college. Rich Handler, CEO of middle-market investment bank Jefferies, once advised that it is never too early to start networking for one's career, and that high-school students could do so too. 
+
+While this is important, we should simultaneously be building a system that instills virtuous moral values in a student's most formative and impressionable years instead of building a system that encourages dismantling it. We have trained high-performing students on a feedback loop involving extrinsic reward, where if students don't succeed by someone arbitrary, poor-defined metric at any point in their formative years, they are a failure. Happiness is solely conditional on success and failure. 
+
+We teach our students Rudyard Kipling's poetry. We should also take a leaf out of Rudyard Kipling's poetry and teach students that:
+
+<div className="border-l-4 border-primary/70 pl-4">
+<div className="text-md">
+
+  "If you can meet with Triumph and Disaster
+
+  And treat those two impostors just the same; 
+
+  ... Yours is the Earth and everything that’s in it,  
+
+  And—which is more—you’ll be a Man, my son!"
+
+  - Rudyard Kipling, "<i>If-</i>"
+</div>
+</div>
+
+<p className="mb-4"></p>
+
+In society, once competition has boiled to a certain point, underhand or immoral tactics will inevitably arise. The applications system does not penalise this phenomenon; in fact, the system even <a href="https://www.wikiwand.com/en/articles/MediSafe_controversy" className="underline hover:text-primary">encourages</a> it in some scenarios. Fortunately these scenarios are often representative of immoral character rather than the immorality of the applications system itself.
+
+In the university application process, success is individual. Students might be involved in their local communities and virtue signal from time to time, but they ultimately understand that to the victor belong the spoils. In life, success is a collaborative endeavour. Those who enter the workforce with the vitriolic, comparative mindset characteristic of many students in the applications game will soon face the wicked slap of reality. In most cases, successful people have found work that matches their strengths, and can inspire others, serve, and lead. It still puzzles me why universities segregate students by metrics completely different from those required from them once they exit university. A reasonable yet unsatisfying answer: "what else can we do"?
+
+I will end this article off by quoting the opening lines of *The Great Gatsby*:
+
+<div className="border-l-4 border-primary/70 pl-4">
+<div className="text-md">
+
+  "In my younger and more vulnerable years my father gave me some advice that I've been turning over in my mind ever since: 'Whenever you feel like criticizing anyone,' he told me, 'just remember that all the people in this world haven't had the advantages that you've had.'".
+</div>
+</div>
 
 
 `
