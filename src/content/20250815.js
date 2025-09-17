5de29d484760e12658570efaef0cdbfa83911ad2
Ethan Choi <ethanchoi@Ethans-MacBook-Pro.local>
2025-09-17 17:47:55 +0800

    sep17

--- src/content/20250815.js
+++ src/content/20250815.js
@@ -4,7 +4,7 @@ export const content = `
 
 The battle to provide rights to same-sex married couples is a contentious one, with both sides simultaneously believing that too much / too little is being done to uphold the rights of same-sex couples. 
 
-In Hong Kong, a contentious bill regarding same-sex couple rights was rejected; said bill merely granted only basic recognitions to same-sex couples, e.g. in bereavement arrangements and necessary medical decisions. Yet many of the arguments against same-sex marriage presented in this case are untenable or are not the strongest contenders against same-sex marriage. I present better arguments that said legislators may have used, as well as the objections to these arguments.
+In Hong Kong, a contentious bill regarding same-sex couple rights was rejected; said bill merely granted only basic recognitions to same-sex couples, e.g. in bereavement arrangements and necessary medical decisions. Yet many of the arguments against same-sex marriage presented in this case are untenable or are not the strongest contenders against same-sex marriage. I quickfire through better arguments that said legislators may have used, as well as the objections to these arguments.
 
 I categorise all arguments against gay marriage as *instrumental*, *intrinsic*, or *religious* arguments. Here I ignore the "foreign conspiracy" argument - where LGBTQ+ rights are controlled by foreign forces to pose a threat to the very fabric of a nation's civilisation. I also ignore nonsensical arguments like the argument that homosexuals cause natural disasters or how fatal tragedies are executions of divine retribution for the intrinsic vice that is homosexuality.
 
@@ -18,23 +18,33 @@ This is an argument used by the likes of right-wing activists which holds some t
 
 The Pride movement was founded on the basis of mutual respect and the recognition that some people live lives different from the extensive majority - a luck-egalitarian argument that societal inequalities such as those commmited against homosexuals are only acceptable if they result from voluntary, responsible choices and not from "bad luck". The minority of institutions who want *reparative* or compensative justice against decades of unequal rights aim to systematically cajole people into believing that more than equality is required. They need presence; they need to be heralded; they need to be lauded in a sense that places them in a socially superior position than non-LGBTQ+ individuals, ceteris paribus. 
 
-In short, this is the "special rights" argument, or the argument that even though the LGBTQ+ movement does not currently represent the minority of activists arguing for special rights, excessive or prolonged exposure to this community would galvanise a non-negligible proportion of activists to argue for special rights.
+In short, this is the "special rights" argument, or the argument that even though the LGBTQ+ movement does not currently represent the minority of activists arguing for special rights, excessive or prolonged exposure to this community would galvanise a non-negligible proportion of activists to argue for special rights. 
 
-This argument is simple to treat. In many widely defended political positions there are individuals who declare their political standpoint as being supreme and right, and thus should be given disproportionate resources to facilitate this. 
+This argument is simple to treat. First, one would have to argue what "special rights" means. Is allowing gay marriage an example of special rights? What about DEI? Who is to say?
 
-<p className="text-xl font-medium"> Gay marriage, as as a part of the LGBTQ+ agenda's indoctrination, leads to an ideology of sexual deviance and impropriety.</p>
+In many widely defended political positions there are individuals who declare their political standpoint as being supreme and right, and thus should be given disproportionate resources to facilitate this. The burden of proof is on the opposition to show that this "loud minority" of activists is any different from the loud minorities of any other political position, and to demonstrate why we should not apply this argument with other political positions with loud and unreasonable minorities. A probable solution is for nations to apply restrictions on the extent and nature of LGBTQ+ rights advocacy for extreme cases, or for major advocacy groups to clearly state the expectations and provisions vis-a-vis LGBTQ+ rights that they aim to fight for.
+
+Another argument under this vein is that societies should encourage what is best for children, which is a mother and a father who are the parents of that child, raising that child in a stable, married relationship.
+
+<p className="text-xl font-medium"> Gay marriage, as as a part of the LGBTQ+ agenda's indoctrination, leads to an ideology of sexual deviance and impropriety, as well as diminish the expectations of parental commitment.</p>
 
 In other words, if we accept gay marriage, then we tacitly accept the normalisation of unbridled licentiousness and sexual deviance. A variant of this argument argues that if we allow gay marriage, then we will have to allow [*insert morally improper policy*], which would be detrimental to society. Examples of improper policies include the legalisation of polygamy, the abolition of the age of consent, and incest.
 
 During the Lavender Scare of the 20th century, the United States government
 
+<p className="text-xl font-medium">If same-sex marriage becomes the norm, we will have much less traditional marriages.</p>
+
 <p className="text-xl font-medium">Many gay rights activists or groups are too forceful with their fight for LGBTQ+ exposure, extensively inserting discourse on gender theory in numerous unrelated contexts and vehemently shunning those who disagree with this large-scale propagation of discourse. Discourse on sexuality and gender should not permeate unrelated contexts.</p>
 
-There is the follow-up thesis that even though most activists are not this forceful, prolonged dialogue on gay marriage and the legality of gay marriage will further goad this permeation. 
+Out of the instrumental arguments (and arguments in general), I believe this is the most tenable.
+
+Examples of "unrelated contexts" include in education, especially early education. How much emphasis, if any, should a teacher put on gay marriage? My personal preference is that early-education educators should place no more emphasis on LGBTQ+ marriages than they do heterosexual marriages. I also prefer for discourse, especially in early education, to touch on factual, impartial topics pertaining to the LGBTQ+ rather than to imbue students with a certain mindset as they should also do with heterosexual matters.
+
+There is the follow-up thesis that even though most activists are not this forceful, prolonged dialogue on gay marriage and the legality of gay marriage will further goad this permeation. See my argument on special rights.
 
 The typical argument against excessive forcefulness is by appealing to Srinivasan's aptness argument. 
 
-Out of the instrumental arguments (and arguments in general), I believe this is the most tenable.
+
 
 
 
@@ -42,13 +52,27 @@ Out of the instrumental arguments (and arguments in general), I believe this is 
 
 These are arguments linked to gay marriage, or homosexuality in general, being an inherent wrong. Note that religious arguments do fall under this category - though the distinction is that religious arguments appeals to supernatural morality, where these arguments appeal to a priori or a posteriori claims.
 
+Some of these arguments are instrumental in nature. The distinction is that intrinsic arguments concern ramifications towards the person.
+
 <p className="text-xl font-medium">The acceptance and legality of gay marriage will harm the social moral order and traditional family values.</p>
+
+n.b. This argument appears to be an instrumental argument, but I classify it as an intrinsic argument as the upshots pertain to the intrinsic ethics of same-sex marriage.
+
+This argument appeals to the "normalness" of heterosexual marriages. 
 
 <p className="text-xl font-medium">Marriage, by definition, should exist as a legal union which benefits society by increasing the population and to raise the youth.</p>
 
 A similar argument goes like this: "Marriage is, tautologically, the union of one man and one woman. Thus the civil rights that gay activists are fighting for is not marriage", or that "gay marriage is like trying to call a dog a cat".
 
 <p className="text-xl font-medium">Procreation is essential for survival and is an objective of marriage. Gay couples cannot procreate as a couple, so gay couples should not be allowed to marry.</p>
+
+<p className="text-xl font-medium">Children have a biological yearning for their fathers / mothers.</p>
+
+<p className="text-xl font-medium">Marriages thrive when spouses take on roles faithful to their gender.</p>
+
+<p className="text-xl font-medium">The burden of proof is on gay couples in proving why they should get married for the same reasons straight couples do - an argument that doesn't just pertain to wants, but pertains to how they benefit society. </p>
+
+This argument seemingly provides justification for why there is a double standard, and why basic rights like marriage, which have been available to heterosexual couples for long, cannot be extended to homosexual couples.
 
 ### Religious arguments
 
